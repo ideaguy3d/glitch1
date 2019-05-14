d7528d93e59c3c7683b95de4be8b6933d42d3bd6
Julius Alvarado <javascript.uiux@gmail.com>
2019-05-13 18:22:14 -0700

    my local copy of the api

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ var schedule = require('node-schedule');
 var Promise = require('promise');
 var escape = require('escape-html');
 var http = require('http'); 
-var serverStartTime = Math.floor(new Date() / 1);
+var cors = require('cors');
 
+var serverStartTime = Math.floor(new Date() / 1);
 
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC, some legacy browsers choke on 204 
+app.use(cors({optionSuccessStatus: 200})); 
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -30,8 +30,10 @@ app.use(express.static('public'));
 // Configure the email transport using the default SMTP transport and a GMail account.
 // See: https://nodemailer.com/
 // For other types of transports (Amazon SES, Sendgrid...) see https://nodemailer.com/2-0-0-beta/setup-transporter/
-var mailTransport = nodemailer.createTransport('smtps://'+process.env.GMAIL_USERNAME+'%40gmail.com:'
-                                               +process.env.GMAIL_PASSWORD+'@smtp.gmail.com');
+var mailTransport = nodemailer.createTransport(
+  'smtps://'+process.env.GMAIL_USERNAME+'%40gmail.com:' +
+  process.env.GMAIL_PASSWORD+'@smtp.gmail.com'
+);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -97,8 +99,15 @@ function sendNotificationToUser(uid, postId) {
   userRef.once('value').then(function(snapshot) {
     var email = snapshot.val().email;
     var postRef = firebase.database().ref('/posts/' + postId);
+
     postRef.once('value').then(function(thepost) {
-      if(!thepost.val().lastNotificationTimestamp || thepost.val().lastNotificationTimestamp>serverStartTime){ // Stop notifications for old stars
+
+      // Stop notifications for old stars
+      if(
+        !thepost.val().lastNotificationTimestamp || 
+        thepost.val().lastNotificationTimestamp>serverStartTime
+      ) {
+
         // Send the email to the user.
         if (email) {
           sendNotificationEmail(email).then(function() {
@@ -107,7 +116,8 @@ function sendNotificationToUser(uid, postId) {
             console.log(reason); // Error
         });
         }
-      } else {
+      } 
+      else {
         updateNotification(uid, postId);
       }
     });
@@ -162,7 +172,9 @@ function startListeners() {
       updateStarCount(firebase.database().ref('user-posts/' + uid + '/' + postId));
       // [END_EXCLUDE]
     }, function(error) {
-      console.log('Failed to add "value" listener at /posts/' + postId + '/stars node:', error);
+      console.log(
+        'Failed to add "value" listener at /posts/' + postId + '/stars node:', error
+      );
     });
     // [END post_value_event_listener]
     // Send email to author when a new star is received.
@@ -170,12 +182,17 @@ function startListeners() {
     postReference.child('stars').on('child_added', function(dataSnapshot) {
       sendNotificationToUser(uid, postId);
     }, function(error) {
-      console.log('Failed to add "child_added" listener at /posts/' + postId + '/stars node:', error);
+      console.log(
+        'Failed to add "child_added" listener at /posts/' + postId + '/stars node:', 
+        error
+      );
     });
     // [END child_event_listener_recycler]
   });
+
   console.log('New star notifier started...');
   console.log('Likes count updater started...');
+  
 }
 
 // Start the Firebase server
